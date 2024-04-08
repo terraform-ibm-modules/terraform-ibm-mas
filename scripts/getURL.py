131c97f03e9a475a87905459fb5951aee960c96d
Nataraj <57617912+NatarajBTI@users.noreply.github.com>
2024-04-08 16:11:29 +0530

    Update getURL.py

--- scripts/getURL.py
+++ scripts/getURL.py
@@ -1,12 +1,13 @@
-
 import json
-import os
+import subprocess
 
 def getAdminURLCore(dep_flavor, instid, wrksid):
-    varstr = os.popen('oc get route -n mas-' + instid + '-core --no-headers | grep admin.' + instid + ' | awk \'{print $2}\'').read().strip()
-    #url = "https://" + varstr
+    admin_route = subprocess.run([f"oc get route {instid}-admin -n mas-{instid}-core -o json"], shell=True, capture_output=True, text=True)
+    json_res = json.loads(admin_route.stdout)
+    admin_host = json_res.get("spec").get("host")
+    admin_url = f"https://{admin_host}"
     data = {
-        "admin_url": varstr
+        "admin_url": admin_url
         }
     # Encode the dictionary into JSON format
     json_output = json.dumps(data)
