6e11e63ef8fe357eb00136e8eaaf428d590c40c2
Nataraj <57617912+NatarajBTI@users.noreply.github.com>
2024-04-09 21:46:08 +0530

    Update getURL.py

--- scripts/getURL.py
+++ scripts/getURL.py
@@ -46,6 +46,49 @@ def getAdminURLCore(kube_config, instid):
         print(f"Error: Failed to execute command: {e}")
         sys.exit(5)
 
+def getAdminURLManage(kube_config, instid):
+    try:
+        process = subprocess.Popen(['oc', 'get', 'route',
+                                    '-n', f'mas-{instid}-manage',
+                                    '-o', 'json',
+                                    '--kubeconfig', kube_config],
+                                   stdout=subprocess.PIPE, universal_newlines=True)
+
+        output, _ = process.communicate()
+
+        if process.returncode != 0:
+            print("Error: Failed to execute 'oc get route' command")
+            sys.exit(1)
+
+        data = json.loads(output)
+        routes = data.get('items', [])
+
+        for route in routes:
+            if f'{workspaceId}-all.{instid}' in route['spec']['host']:
+                varstr = route['spec']['host']
+                break
+        else:
+            print(f"Error: No route found for 'admin.{instid}'")
+            sys.exit(2)
+
+        result = {
+            "admin_url": f"https://{varstr}/maximo"
+        }
+        json_output = json.dumps(result)
+        print(json_output)
+
+    except json.JSONDecodeError as e:
+        print(f"Error: Failed to parse JSON: {e}")
+        sys.exit(3)
+
+    except subprocess.CalledProcessError as e:
+        print(f"Error: Command '{e.cmd}' returned non-zero exit status {e.returncode}")
+        sys.exit(4)
+
+    except OSError as e:
+        print(f"Error: Failed to execute command: {e}")
+        sys.exit(5)
+
 
 if __name__ == "__main__":
     # get KUBECONFIG containing path from json passed to the command as an argument
@@ -55,4 +98,15 @@ if __name__ == "__main__":
     # get the KUBECONFIG path from the json
     kubeconfig = input_json['KUBECONFIG']
 
-    getAdminURLCore(kube_config=kubeconfig, instid="natinst1")
+    #instanceId = sys.argv[1]
+    #capability = sys.argv[2]
+    #workspaceId = sys.argv[3]
+
+    instanceId = natinst1
+    capability = core
+    workspaceId = wrkid1
+    
+    if capability == "core":
+        getAdminURLCore(kube_config=kubeconfig, instid=instanceId)
+    elif capability == "manage":
+        getAdminURLManage(kube_config=kubeconfig, instid=instanceId,workspaceId=workspaceId)
