2096ee9ffcc87a8724117013e735e6cb4c92b89c
Nataraj <57617912+NatarajBTI@users.noreply.github.com>
2024-04-10 23:36:39 +0530

    Update installVerify.sh

--- scripts/installVerify.sh
+++ scripts/installVerify.sh
@@ -1,65 +1,83 @@
-set -e
+import json
+import subprocess
+import sys
+import time
 
-sleep 300
+def verifyPipelineStatus(kube_config, instid, capability):
+    RETRY_COUNT = 0
+    TIME_TO_WAIT = 0
+    if capability == "core":
+        RETRY_COUNT = 30
+        TIME_TO_WAIT = 180
+    elif capability == "manage":
+        RETRY_COUNT = 60
+        TIME_TO_WAIT = 300
+    
+    
+      
+    try:
+        pipline_status= "Failed Retrying, Pipeline still in Running state "
+        for interval_count in range(RETRY_COUNT):
+            process = subprocess.Popen(['oc', 'get', 'pr',
+                                        '-n', f'mas-{instid}-pipelines',
+                                        '-o', 'json',
+                                        '--kubeconfig', kube_config],
+                                    stdout=subprocess.PIPE, universal_newlines=True)
 
-# Function to track the status of MAS Core+Manage pipeline and to exit in case of failure and to wait for all retries in case if the pipeline is still running.
-function verifyPipelineStatusManage()
-{
-for (( i=0; i<=60; i++ ));
-        do
-        varstr3=$(oc get pr -n mas-${var2}-pipelines | awk -F' ' '{print $3}')
-        varstr3=$(echo $varstr3 | cut -d ' ' -f 1)
+            output, _ = process.communicate()
 
-        varstr4=$(oc get pr -n mas-${var2}-pipelines | awk -F' ' '{print $3}')
-        varstr4=$(echo $varstr4 | cut -d ' ' -f 2)
+            if process.returncode != 0:
+                print("Error: Failed to execute 'oc get route' command")
+                sys.exit(1)
 
-        if [[ $varstr3 == "REASON" && $varstr4 == "Completed"  ]]; then
-                echo -n '{"PipelineRunStatus":"'"Successful"'"}' > result.txt
-                chmod +x result.txt
-                break
-        elif [[ $varstr3 == "REASON" && $varstr4 == "Running"  ]]; then
-                sleep 300
-        elif [[ $varstr3 == "REASON" && $varstr4 == "Failed"  ]]; then
-        varstr5=$(oc get taskrun -A -n mas-${var2}-pipelines | grep Failed | awk -F' ' '{print $2}')
-                varstr6=$varstr5"_failed"
-                echo -n '{"PipelineRunStatus":"'"${varstr6}"'"}' > result.txt
-                chmod +x result.txt
-                break
-        fi
-        done
-}
+            data = json.loads(output)
+            pipeline_runs = data.get('items', [])
+            
+            pipeline_run = None
+            
+            if len(pipeline_runs) > 0:
+                pipeline_run = pipeline_runs[0]
+            
+            if pipeline_run:
 
-# Function to track the status of MAS Core pipeline and to exit in case of failure and to wait for all retries in case if the pipeline is still running.
-function verifyPipelineStatusCore()
-{
-for (( i=0; i<=30; i++ ));
-        do
-        varstr3=$(oc get pr -n mas-${var2}-pipelines | awk -F' ' '{print $3}')
-        varstr3=$(echo $varstr3 | cut -d ' ' -f 1)
+                pipeline_status_reason = pipeline_run.get('status').get('conditions')[0].get('reason')
+                
+                if pipeline_status_reason == "Completed":
+                    pipline_status = "Successful"
+                    break
+                    
+                elif pipeline_status_reason == "Running":
+                    time.sleep(TIME_TO_WAIT)
+                    pass
+                elif pipeline_status_reason == "Failed":
+                    pipline_status = pipeline_run.get('status')[0].get('message')
+                    break
+        result = {
+            "PipelineRunStatus":pipline_status
+            }
+        json_output = json.dumps(result)
+        print(json_output)
 
-        varstr4=$(oc get pr -n mas-${var2}-pipelines | awk -F' ' '{print $3}')
-        varstr4=$(echo $varstr4 | cut -d ' ' -f 2)
+    except json.JSONDecodeError as e:
+        print(f"Error: Failed to parse JSON: {e}")
+        sys.exit(3)
 
-        if [[ $varstr3 == "REASON" && $varstr4 == "Completed"  ]]; then
-                echo -n '{"PipelineRunStatus":"'"Successful"'"}' > result.txt
-                chmod +x result.txt
-                break
-        elif [[ $varstr3 == "REASON" && $varstr4 == "Running"  ]]; then
-                sleep 180
-        elif [[ $varstr3 == "REASON" && $varstr4 == "Failed"  ]]; then
-        varstr5=$(oc get taskrun -A -n mas-${var2}-pipelines | grep Failed | awk -F' ' '{print $2}')
-                varstr6=$varstr5"_failed"
-                echo -n '{"PipelineRunStatus":"'"${varstr6}"'"}' > result.txt
-                chmod +x result.txt
-                break
-        fi
-        done
-}
-var1=$1
-var2=$2
+    except subprocess.CalledProcessError as e:
+        print(f"Error: Command '{e.cmd}' returned non-zero exit status {e.returncode}")
+        sys.exit(4)
 
-if [[ $var1 == "core" ]]; then
-verifyPipelineStatusCore
-elif [[ $var1 == "manage" ]]; then
-verifyPipelineStatusManage
-fi
+    except OSError as e:
+        print(f"Error: Failed to execute command: {e}")
+        sys.exit(5)
+
+
+if __name__ == "__main__":
+    # get KUBECONFIG containing path from json passed to the command as an argument
+    # Read the JSON input from stdin
+    input_json = json.loads(sys.stdin.read())
+
+    # get the KUBECONFIG path from the json
+    kubeconfig = input_json['KUBECONFIG']
+    
+    verifyPipelineStatus(kube_config=kubeconfig, instid=sys.argv[1],capability=sys.argv[2])
+    #verifyPipelineStatus(kube_config=kubeconfig, instid="natinst2",capability="manage")
