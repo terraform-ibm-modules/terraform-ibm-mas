88125adf51097ba3d9c01a7de5bb51fe275b238a
Daniel Butler <dannyeb@gmail.com>
2024-04-25 11:10:55 +0100

    update comment

--- solutions/existing-cluster/version.tf
+++ solutions/existing-cluster/version.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.3.0, <1.7.0"
   required_providers {
-    # Use a range in modules
+    # Lock DA into an exact provider version - renovate automation will keep it updated
     ibm = {
       source  = "ibm-cloud/ibm"
       version = "1.64.2"
