7771da9cb565bf732e6cd9c26f97beff5c34d652
NATARAJSHIVASHANKARA <NATARAJSHIVASHANKARA@LAPTOP-69F31GB6>
2024-05-01 05:33:15 +0530

    changes..

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -2,13 +2,13 @@
 # SLZ OCP
 ##############################################################################
 
-data "ibm_container_cluster_versions" "cluster_versions" {
-}
+#data "ibm_container_cluster_versions" "cluster_versions" {
+#}
 
-locals {
+#locals {
   #default_ocp_version = "${data.ibm_container_cluster_versions.cluster_versions.default_openshift_version}_openshift"
-  ocp_version         = var.ocp_version == null || var.ocp_version == "default" ? local.default_ocp_version : "${var.ocp_version}_openshift"
-}
+  #ocp_version         = var.ocp_version == null || var.ocp_version == "default" ? local.default_ocp_version : "${var.ocp_version}_openshift"
+#}
 
 module "landing_zone" {
   source  = "terraform-ibm-modules/landing-zone/ibm//patterns//roks//module"
