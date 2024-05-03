f11478db706bb78889ab0e5ede9bd423b980675f
ocofaigh <ocofaigh@ie.ibm.com>
2024-05-03 16:48:11 +0100

    test: add correct workaround

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -56,13 +56,7 @@ func setupOptions(t *testing.T, prefix string, dir string, terraformVars map[str
 		TerraformDir:  dir,
 		Prefix:        fmt.Sprintf("%s-%s", prefix, strings.ToLower(random.UniqueId())),
 		TerraformVars: terraformVars,
-		// Workaround for https://github.com/terraform-ibm-modules/terraform-ibm-mas/issues/78
-		ImplicitRequired: true,
-		ImplicitDestroy: []string{
-			"module.landing_zone.module.landing_zone.ibm_resource_group.resource_groups",
-		},
-		// Ignore for consistency check since they are set to always run in the code
-		IgnoreDestroys: testhelper.Exemptions{
+		IgnoreDestroys: testhelper.Exemptions{ // Ignore for consistency check since they are set to always run in the code
 			List: []string{
 				"module.existing_cluster.null_resource.install_verify",
 				"module.existing_cluster.null_resource.maximo_admin_url",
@@ -149,7 +143,13 @@ func TestRunDACore(t *testing.T) {
 		assert.True(t, setupErr == nil, "Setup DA basic failed")
 		return
 	}
-	defer terraform.Destroy(t, preReqOptions)
+
+	// Workaround for https://github.com/terraform-ibm-modules/terraform-ibm-mas/issues/78
+	// defer terraform.Destroy(t, preReqOptions)
+	defer func() {
+		terraform.RunTerraformCommand(t, preReqOptions, "state", "rm", "module.landing_zone.module.landing_zone.ibm_resource_group.resource_groups[\"workload-rg\"]")
+		terraform.Destroy(t, preReqOptions)
+	}()
 
 	output, err := options.RunTestConsistency()
 	assert.Nil(t, err, "This should not have errored")
