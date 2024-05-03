2f8186c436920d4e8bd6cd491dc4b42f2eb21b65
Nataraj <57617912+NatarajBTI@users.noreply.github.com>
2024-05-03 16:14:09 +0530

    changing to dynamic MAS operator catalog..

--- solutions/existing-cluster/main.tf
+++ solutions/existing-cluster/main.tf
@@ -2,13 +2,13 @@ module "existing_cluster" {
   source                       = "../../"
   cluster_id                   = var.cluster_id
   deployment_flavour           = var.deployment_flavour
-  entitlement_key          = var.entitlement_key
+  entitlement_key              = var.entitlement_key
   mas_instance_id              = var.mas_instance_id
   mas_license                  = var.mas_license
   sls_license_id               = var.sls_license_id
-  contact_email            = var.contact_email
-  contact_firstname        = var.contact_firstname
-  contact_lastname         = var.contact_lastname
+  contact_email                = var.contact_email
+  contact_firstname            = var.contact_firstname
+  contact_lastname             = var.contact_lastname
   cluster_config_endpoint_type = var.cluster_config_endpoint_type
   mas_workspace_id             = var.mas_workspace_id
   mas_workspace_name           = var.mas_workspace_name
