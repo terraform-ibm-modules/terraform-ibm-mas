d921a3948518b12e5cf3670226058ae611ac34f2
Nataraj <nataraj.s@in.ibm.com>
2024-05-07 15:43:06 +0530

    lint issue fixes..

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -2,9 +2,6 @@
 # SLZ OCP
 ##############################################################################
 
-data "ibm_container_cluster_versions" "cluster_versions" {
-}
-
 module "landing_zone" {
   source   = "terraform-ibm-modules/landing-zone/ibm//patterns//roks//module"
   version  = "5.21.0"
