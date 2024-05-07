55b4a336daa00668c65b8fe417ff9816532bf5c8
Nataraj <nataraj.s@in.ibm.com>
2024-05-07 14:13:05 +0530

    changes..

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -8,7 +8,7 @@ module "mas_core" {
   entitlement_key              = var.entitlement_key
   mas_license                  = var.mas_license
   sls_license_id               = var.sls_license_id
-  deployment_flavor           = var.deployment_flavor
+  deployment_flavor            = var.deployment_flavor
   mas_instance_id              = var.mas_instance_id
   contact_email                = var.contact_email
   contact_firstname            = var.contact_firstname
