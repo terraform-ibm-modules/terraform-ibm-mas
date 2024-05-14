81a294c93f76f2100c2b95c5d56690372d182023
Nataraj <nataraj.s@in.ibm.com>
2024-05-14 16:31:09 +0530

    changes..

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -3,7 +3,7 @@
 ##############################################################################
 
 locals {
-  override_json_path = abspath("../../override-json-file/override.json")
+  override_json_path = "${path.module}/override-json-file/override.json"
 }
 
 module "landing_zone" {
