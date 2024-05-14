fafdcf70b03d50f20d3f6d28d33db6f9f0f90d35
Nataraj <nataraj.s@in.ibm.com>
2024-05-14 15:07:28 +0530

    changes to override.json file path..

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -3,10 +3,11 @@
 ##############################################################################
 
 module "landing_zone" {
-  source   = "terraform-ibm-modules/landing-zone/ibm//patterns//roks//module"
-  version  = "5.21.1"
-  region   = var.region
-  prefix   = var.prefix
-  tags     = var.resource_tags
-  override = true
+  source             = "terraform-ibm-modules/landing-zone/ibm//patterns//roks//module"
+  version            = "5.21.1"
+  region             = var.region
+  prefix             = var.prefix
+  tags               = var.resource_tags
+  override_json_path = "../../override-json-file/override.json"
+  override           = true
 }
