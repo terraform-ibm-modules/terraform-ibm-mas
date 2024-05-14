72a6098cd75914e812bec1e9e9ff6c91aea3997e
Nataraj <nataraj.s@in.ibm.com>
2024-05-14 16:15:46 +0530

    changes..

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -2,11 +2,16 @@
 # SLZ OCP
 ##############################################################################
 
+locals {
+  override_json_path = abspath("../../override-json-file/override.json")
+}
+
 module "landing_zone" {
-  source   = "terraform-ibm-modules/landing-zone/ibm//patterns//roks//module"
-  version  = "5.21.1"
-  region   = var.region
-  prefix   = var.prefix
-  tags     = var.resource_tags
-  override = true
+  source             = "terraform-ibm-modules/landing-zone/ibm//patterns//roks//module"
+  version            = "5.21.1"
+  region             = var.region
+  prefix             = var.prefix
+  tags               = var.resource_tags
+  override           = true
+  override_json_path = local.override_json_path
 }
