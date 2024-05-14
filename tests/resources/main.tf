20a24f3b0bd6c610967e3d554d8303272c2f80cd
Nataraj <nataraj.s@in.ibm.com>
2024-05-14 22:21:08 +0530

    reverting back the path changes..

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -2,16 +2,11 @@
 # SLZ OCP
 ##############################################################################
 
-locals {
-  override_json_path = "${path.root}/../../override-json-file/override.json"
-}
-
 module "landing_zone" {
-  source             = "terraform-ibm-modules/landing-zone/ibm//patterns//roks//module"
-  version            = "5.21.1"
-  region             = var.region
-  prefix             = var.prefix
-  tags               = var.resource_tags
-  override           = true
-  override_json_path = local.override_json_path
+  source   = "terraform-ibm-modules/landing-zone/ibm//patterns//roks//module"
+  version  = "5.21.1"
+  region   = var.region
+  prefix   = var.prefix
+  tags     = var.resource_tags
+  override = true
 }
