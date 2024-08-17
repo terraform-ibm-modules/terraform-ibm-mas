4baa356e7469343fa386b6fcfbbd883e97476a7f
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-08-17 15:56:16 +0100

    chore(deps): update terraform terraform-ibm-modules/landing-zone/ibm to v5.29.0 (#118)

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -4,7 +4,7 @@
 
 module "landing_zone" {
   source   = "terraform-ibm-modules/landing-zone/ibm//patterns//roks//module"
-  version  = "5.22.0"
+  version  = "5.29.0"
   region   = var.region
   prefix   = var.prefix
   tags     = var.resource_tags
